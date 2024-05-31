655a4755f02266fc817e5f1652453cb8a3c157db
david2242 <88514735+david2242@users.noreply.github.com>
2024-05-31 23:50:53 +0200

    1 - Added nx workspace angular default frontend dotnet default backend

---
+++ workspace/apps/backend-test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Workspace.Backend.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
