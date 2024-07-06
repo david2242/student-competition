19ed05c9ff8948a23e7e1ff5564d58161dc0624e
david2242 <88514735+david2242@users.noreply.github.com>
2024-07-06 08:03:22 +0200

    7 - Added EF, created db, migration, refactored service methods

---
+++ workspace/apps/backend/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Workspace.Backend.Models;
+
+namespace Workspace.Backend.Data
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Competition> Competitions => Set<Competition>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      // Configure Competition to own Result
+      modelBuilder.Entity<Competition>()
+        .OwnsOne(c => c.Result, r =>
+        {
+          r.Property(p => p.Position);
+          r.Property(p => p.SpecialPrize);
+          r.Property(p => p.Compliment);
+          r.Property(p => p.NextRound);
+        });
+    }
+  }
+}
