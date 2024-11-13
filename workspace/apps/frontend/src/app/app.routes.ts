15d281e8f5ed9223ef601ccdc17939b775045411
david2242 <88514735+david2242@users.noreply.github.com>
2024-11-13 10:48:58 +0100

    32 - Created NAV, Competition Editor

--- workspace/apps/frontend/src/app/app.routes.ts
+++ workspace/apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
+import {CompetitionListComponent} from "./components/pages/competition/competition-list/competition-list.component";
+import {
+  CompetitionEditorComponent
+} from "./components/pages/competition/competition-editor/competition-editor.component";
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'competition', component: CompetitionListComponent, pathMatch: 'full' },
+  { path: 'competition-editor', component: CompetitionEditorComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'competition', pathMatch: 'full' },
+];
