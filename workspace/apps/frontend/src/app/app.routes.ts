9380c01c4820a46164b2fc35acf3cf803237f2d4
David Nagy <plusz.egy.cim@gmail.com>
2025-03-22 23:52:55 +0100

    48 - Added welcome page

--- workspace/apps/frontend/src/app/app.routes.ts
+++ workspace/apps/frontend/src/app/app.routes.ts
@@ -4,11 +4,12 @@ import {
   CompetitionEditorComponent
 } from "@/app/components/pages/competition/competition-editor/competition-editor.component";
 import { LoginComponent } from "@/app/components/login/login.component";
+import { WelcomeComponent } from "@/app/components/pages/welcome/welcome.component";
 
 export const appRoutes: Route[] = [
   { path: 'competition', component: CompetitionListComponent },
   { path: 'competition/:id', component: CompetitionEditorComponent },
   { path: 'competition-editor', component: CompetitionEditorComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'competition', pathMatch: 'full' },
+  { path: '', component: WelcomeComponent },
 ];
