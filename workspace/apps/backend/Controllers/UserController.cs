dbe20fbc0c5e3edd49d51ef290024c83af4676c6
David Nagy <plusz.egy.cim@gmail.com>
2026-03-03 23:59:47 +0100

    105 - Centralize exceptions and use ExceptionMiddleware to remove try-catch blocs

--- workspace/apps/backend/Controllers/UserController.cs
+++ workspace/apps/backend/Controllers/UserController.cs
@@ -1,140 +1,62 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 using Workspace.Backend.Dtos.User;
 using Workspace.Backend.Models;
 using Workspace.Backend.Services.UserService;
 
-namespace Workspace.Backend.Controllers
+namespace Workspace.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  public class UserController : ControllerBase
-  {
     private readonly IUserService _userService;
     private readonly UserManager<IdentityUser> _userManager;
 
     public UserController(
-      IUserService userService,
-      UserManager<IdentityUser> userManager
+        IUserService userService,
+        UserManager<IdentityUser> userManager
     )
     {
-      _userService = userService;
-      _userManager = userManager;
+        _userService = userService;
+        _userManager = userManager;
     }
 
     [HttpGet(""), Authorize(Roles = "admin")]
-    [Authorize]
-    public async Task<ActionResult<ServiceResponse<List<GetUserResponseDto>>>> GetAll()
+    public async Task<IActionResult> GetAll()
     {
-      var serviceResponse = new ServiceResponse<List<GetUserResponseDto>>();
-      try
-      {
-        serviceResponse.Data = await _userService.GetAll();
-        return Ok(serviceResponse);
-      }
-      catch (Exception e)
-      {
-        serviceResponse.Data = null;
-        serviceResponse.Success = false;
-        serviceResponse.Message = e.Message;
-        return BadRequest(serviceResponse);
-      }
+        var result = await _userService.GetAll();
+        return Ok(new ServiceResponse<List<GetUserResponseDto>> { Data = result });
     }
 
     [HttpGet("{id}"), Authorize(Roles = "admin")]
-    public async Task<ActionResult<ServiceResponse<GetUserResponseDto>>> GetSingle(string id)
+    public async Task<IActionResult> GetSingle(string id)
     {
-      var serviceResponse = new ServiceResponse<GetUserResponseDto>();
-      try
-      {
-        serviceResponse.Data = await _userService.GetSingle(id);
-        return Ok(serviceResponse);
-      }
-      catch (KeyNotFoundException e)
-      {
-        serviceResponse.Data = null;
-        serviceResponse.Success = false;
-        serviceResponse.Message = e.Message;
-        return NotFound(serviceResponse);
-      }
-      catch (Exception e)
-      {
-        serviceResponse.Data = null;
-        serviceResponse.Success = false;
-        serviceResponse.Message = e.Message;
-        return BadRequest(serviceResponse);
-      }
+        var result = await _userService.GetSingle(id);
+        return Ok(new ServiceResponse<GetUserResponseDto> { Data = result });
     }
 
     [HttpPost, Authorize(Roles = "admin")]
-    public async Task<ActionResult<ServiceResponse<List<GetUserResponseDto>>>> AddUser(AddUserRequestDto newUser)
+    public async Task<IActionResult> AddUser(AddUserRequestDto newUser)
     {
-      var serviceResponse = new ServiceResponse<List<GetUserResponseDto>>();
-      try
-      {
-        serviceResponse.Data = await _userService.AddUser(newUser);
-        return Ok(serviceResponse);
-      }
-      catch (Exception e)
-      {
-        serviceResponse.Data = null;
-        serviceResponse.Success = false;
-        serviceResponse.Message = e.Message;
-        return BadRequest(serviceResponse);
-      }
+        var result = await _userService.AddUser(newUser);
+        return Ok(new ServiceResponse<List<GetUserResponseDto>> { Data = result });
     }
 
     [HttpPut("{id}"), Authorize(Roles = "admin")]
-    public async Task<ActionResult<ServiceResponse<GetUserResponseDto>>> UpdateUser(string id,
-      UpdateUserRequestDto updatedUser)
+    public async Task<IActionResult> UpdateUser(string id, UpdateUserRequestDto updatedUser)
     {
-      var serviceResponse = new ServiceResponse<List<GetUserResponseDto>>();
-      try
-      {
-        serviceResponse.Data = await _userService.UpdateUser(id, updatedUser);
-        return Ok(serviceResponse);
-      }
-      catch (KeyNotFoundException e)
-      {
-        serviceResponse.Data = null;
-        serviceResponse.Success = false;
-        serviceResponse.Message = e.Message;
-        return NotFound(serviceResponse);
-      }
-      catch (Exception e)
-      {
-        serviceResponse.Data = null;
-        serviceResponse.Success = false;
-        serviceResponse.Message = e.Message;
-        return BadRequest(serviceResponse);
-      }
+        var result = await _userService.UpdateUser(id, updatedUser);
+        return Ok(new ServiceResponse<List<GetUserResponseDto>> { Data = result });
     }
 
     [HttpDelete("{id}"), Authorize(Roles = "admin")]
-    public async Task<ActionResult<ServiceResponse<List<GetUserResponseDto>>>> DeleteUser(string id)
+    public async Task<IActionResult> DeleteUser(string id)
     {
-      var serviceResponse = new ServiceResponse<List<GetUserResponseDto>>();
-      try
-      {
-        serviceResponse.Data = await _userService.DeleteUser(id);
-        return Ok(serviceResponse);
-      }
-      catch (KeyNotFoundException e)
-      {
-        serviceResponse.Data = null;
-        serviceResponse.Success = false;
-        serviceResponse.Message = e.Message;
-        return NotFound(serviceResponse);
-      }
-      catch (Exception e)
-      {
-        serviceResponse.Data = null;
-        serviceResponse.Success = false;
-        serviceResponse.Message = e.Message;
-        return BadRequest(serviceResponse);
-      }
+        var result = await _userService.DeleteUser(id);
+        return Ok(new ServiceResponse<List<GetUserResponseDto>> { Data = result });
     }
-  }
-};
+}
