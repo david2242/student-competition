a956ce9cce95593716a7feb24fbfd8cc1d3c10e2
David Nagy <plusz.egy.cim@gmail.com>
2026-03-04 00:00:31 +0100

    105 - Add tests and integration tests

--- workspace/apps/backend/Controllers/DebugController.cs
+++ workspace/apps/backend/Controllers/DebugController.cs
@@ -1,19 +1,41 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Workspace.Backend.Exceptions;
 
-namespace Workspace.Backend.Controllers
-{
-
+namespace Workspace.Backend.Controllers;
 
 [ApiController]
 [Route("api/test")]
 public class TestController : ControllerBase
 {
-  [HttpGet("public")]
-  public IActionResult Public() => Ok("Public endpoint");
+    [HttpGet("public")]
+    public IActionResult Public() => Ok("Public endpoint");
 
-  [HttpGet("protected")]
-  [Authorize]
-  public IActionResult Protected() => Ok("Protected endpoint");
-}
+    [HttpGet("protected")]
+    [Authorize]
+    public IActionResult Protected() => Ok("Protected endpoint");
+
+    [HttpGet("notfound")]
+    public IActionResult ThrowNotFound()
+    {
+        throw new NotFoundException("Test NotFound Exception");
+    }
+
+    [HttpGet("unauthorized")]
+    public IActionResult ThrowUnauthorized()
+    {
+        throw new UnauthorizedAccessException("Test Unauthorized Exception");
+    }
+
+    [HttpGet("validation")]
+    public IActionResult ThrowValidation()
+    {
+        throw new ValidationException("Test Validation Exception");
+    }
+
+    [HttpGet("error")]
+    public IActionResult ThrowGeneral()
+    {
+        throw new Exception("Test General Exception");
+    }
 }
